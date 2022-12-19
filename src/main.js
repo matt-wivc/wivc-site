faf56ae4a1b67c261030d53b9c5941f929f264b9
Matthew <matthew.sanford@rebilly.com>
2022-12-19 12:53:03 -0500

    use vite

--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
-import router from './router'
-import store from './store'
 
-Vue.config.productionTip = false
-
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+createApp(App).mount('#app')
