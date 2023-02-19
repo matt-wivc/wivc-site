e0c614a09cbf529381524f976b863af3e538c801
Matt Sanford <matt.san21@gmail.com>
2023-02-19 12:22:31 -0500

    fix images

---
+++ src/components/Icon.vue
@@ -0,0 +1,13 @@
+<script setup>
+import {defineProps} from 'vue';
+
+const props = defineProps({
+  icon: String
+});
+</script>
+
+<template>
+  <span class="material-symbols-rounded">
+    <slot>{{ icon }}</slot>
+  </span>
+</template>
